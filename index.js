cfc0fab41bae5a3cd3b10ebbaaf39e2f46682405
aaron549 <sunaaron549@gmail.com>
2021-04-12 12:12:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -76,15 +76,64 @@ const titles = [
 
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
+const wrote = books.map(
+  (book) =>
+    book.authorFirst +
+    " " +
+    book.authorLast +
+    " wrote " +
+    book.name +
+    " in " +
+    book.publishDate
+);
 
+console.log(wrote);
 //Sort books from oldest to most recent
+const ordered = books.sort(function (a, b) {
+  if (a.publishDate > b.publishDate) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+console.table(ordered);
+
+//can also do const ordered = books.sort((a,b) => a.publishdate > b.year ? 1: -1);
 
 //sort books alphabetically
-
+const alphabetically = books.sort((book) => book.name);
+console.table(alphabetically);
 //Find who wrote War and Peace
 
+const author = books.find(function (comment) {
+  if (comment.name === "War and Peace") {
+    return true;
+  }
+});
+console.log(author);
+
 //how many books were written before 1900?
+const written = books.filter((name) => name.publishDate < 1900);
+console.table(written);
+
+//const written = books.filter(function(name) {
+//if(name.publishDate < 1900) {
+//return true;
+//}
+//});//OLD
+
+//console.table(written);
 
 //was there at least one book published within the last 100 years?
-
+//const published = books.some(function (publishDate) {
+//const currentYear = new Date().getFullYear();
+//if (currentYear - publishDate.year <= 100) {
+//return true;
+//}
+//});
+const published = books.some((publishDate) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - publishDate.year <= 100;
+});
+console.log({ published });
 //was every book published within the last 100 years?
